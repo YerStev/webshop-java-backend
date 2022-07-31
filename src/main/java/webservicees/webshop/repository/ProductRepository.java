9119595f6a57adef1c71421b210e664a815e8661
StevenD <steven.duque@outlook.de>
2022-07-31 13:18:41 +0200

    Extract methode lowerCaseTags

--- src/main/java/webservicees/webshop/repository/ProductRepository.java
+++ src/main/java/webservicees/webshop/repository/ProductRepository.java
@@ -8,50 +8,55 @@ import java.util.List;
 
 public class ProductRepository {
 
-    public List <ProductResponse> getAll(String tag){
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "Hemd",
-                        "sau gute beschreibung",
-                        1234,
-                        Arrays.asList("kleidung", "oversize")
-                ),
-                new ProductResponse(
-                        "2",
-                        "hose",
-                        "sau gute beschreibung",
-                        1234,
-                        Arrays.asList("kleidung", "regular")
-                ),
-                new ProductResponse(
-                        "3",
-                        "tshirt",
-                        "sau gute beschreibung",
-                        1234,
-                        Arrays.asList("kleidung", "oversize")
-                ),
-                new ProductResponse(
-                        "4",
-                        "shorts",
-                        "sau gute beschreibung",
-                        1234,
-                        Arrays.asList("kleidung", "slim")
-                )
-        );
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "Hemd",
+                    "sau gute beschreibung",
+                    1234,
+                    Arrays.asList("kleidung", "oversize")
+            ),
+            new ProductResponse(
+                    "2",
+                    "hose",
+                    "sau gute beschreibung",
+                    1234,
+                    Arrays.asList("kleidung", "regular")
+            ),
+            new ProductResponse(
+                    "3",
+                    "tshirt",
+                    "sau gute beschreibung",
+                    1234,
+                    Arrays.asList("kleidung", "oversize")
+            ),
+            new ProductResponse(
+                    "4",
+                    "shorts",
+                    "sau gute beschreibung",
+                    1234,
+                    Arrays.asList("kleidung", "slim")
+            )
+    );
+
+    public List<ProductResponse> getAll(String tag) {
+
 
         if (tag == null) return products;
         tag = tag.toLowerCase();
         List<ProductResponse> filtered = new ArrayList<>();
 
-        for (ProductResponse p : products){
-            List <String> lowerCaseTags = new ArrayList<>();
-            for(String t : p.getTags())
-                lowerCaseTags.add(t.toLowerCase());
-
-            if (lowerCaseTags.contains(tag))
+        for (ProductResponse p : products) {
+            if (lowerCaseTags(p).contains(tag))
                 filtered.add(p);
         }
         return filtered;
+    }
+
+    private static List<String> lowerCaseTags(ProductResponse p) {
+        List<String> lowerCaseTags = new ArrayList<>();
+        for (String t : p.getTags())
+            lowerCaseTags.add(t.toLowerCase());
+        return lowerCaseTags;
     }
 }
